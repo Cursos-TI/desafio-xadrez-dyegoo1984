13f7b2333df1ac93e2945604710748368be887e4
Diego Oliveira <95553336+dyegoo1984@users.noreply.github.com>
2025-05-02 18:37:29 +0000

    Tema 3 Nivel Novato

--- xadrez.c
+++ xadrez.c
@@ -10,12 +10,27 @@ int main() {
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int casasBispo = 5;
+
+    for(int i = 0; i < casasBispo; i++){
+        printf("[Bispo] Avança! Direita!\n"); 
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int casasTorre = 5;
+
+    for(int i = 0; i < casasTorre; i++){
+        printf("[Torre] Direita! \n");
+    }
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    int casasRainha = 8;
+
+    for(int i = 0; i < casasRainha; i++){
+        printf("[Rainha] Esquerda! \n");
+    }
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
